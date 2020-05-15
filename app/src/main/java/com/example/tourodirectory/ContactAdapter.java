aeadc2c4da4025151631e42fbc954bdb44c06937
AaronF <aaronfarntrog@gmail.com>
2020-05-15 14:58:22 -0400

    initial commit

---
+++ app/src/main/java/com/example/tourodirectory/ContactAdapter.java
@@ -0,0 +1,64 @@
+package com.example.tourodirectory;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHolder> {
+
+    // 1) Store it in a text file and pull it in
+    // 2) from g sheet
+    // 3) Store it in an array
+    private String[] mContacts;
+
+
+    // Constructor
+    public ContactAdapter() {
+        mContacts = new String[]{"Professor A", "Professor B", "Professor C"};
+    }
+
+
+    // STEP 1
+    // this will be called automatically to essentially construct each individual contact, which is a ViewHolder
+    // inflates and then returns a copy of our rv_item.xml to the RecyclerView!
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // INFLATE our contact.xml
+        View itemLayoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.contact, parent, false);
+        return new ViewHolder(itemLayoutView);
+    }
+
+    // STEP 3
+    // – this will also be called automatically after onCreateViewHolder() to bind this square to its data source
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.mButton.setText(mContacts[position]);
+    }
+
+
+    // Our RecyclerView calls this getItemCount() to know how many items there are to inflate
+    @Override
+    public int getItemCount() {
+        return mContacts.length;
+    }
+
+
+    // STEP 2
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        // Contact button
+        private Button mButton;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            // sets the button to be used in the onBindViewHolder()
+            mButton = itemView.findViewById(R.id.contact_button);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tourodirectory/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.tourodirectory;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Our Adapter
+    private ContactAdapter mContactAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Setup
+        setupToolbar();
+        setupFAB();
+
+        // create/setup RecyclerView
+        setupDirectory();
+        
+    }
+
+    private void setupDirectory() {
+
+        // Get the recyclerview element from our content_main.xml file.
+        RecyclerView objRecyclerView = findViewById(R.id.recycler_view);
+        // Our contact list does not change once it is created in the adapter/backend
+        objRecyclerView.setHasFixedSize(true);
+
+        // This sets up our vertical directory
+        RecyclerView.LayoutManager objLayoutManager = new LinearLayoutManager(this);
+
+        // Instantiate our ContactAdapter (and optionally pass in our size )
+        mContactAdapter = new ContactAdapter();
+
+        // set the RecyclerView to use the layoutmanager
+        objRecyclerView.setLayoutManager(objLayoutManager);
+
+        // set the recyclerview to use the adapter
+        objRecyclerView.setAdapter(mContactAdapter);
+    }
+
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+
+    private void setupFAB() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
